6c9c8ec07a9dc0ac8c37718ceb60f45b4b2c9b46
WU, TA-YU <wu30030@gmail.com>
2026-08-25 09:03:56 +0800

    feat: connect frontend to cloud backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,10 +1,5 @@
-/**
- * API Server 設定
- * 未來只要修改這裡即可切換測試機或正式機
- */
+﻿const CONFIG = {
 
-const CONFIG = {
-
-    API_BASE_URL: "http://localhost:3000"
+    API_BASE_URL: "https://transportation-system-api.onrender.com"
 
 };
